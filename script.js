b2fcf56854080831ba9a47f08db20d5daa13b4c9
Vishakha02D2 <120273836+Vishakha02D2@users.noreply.github.com>
2023-06-30 23:46:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
-function letterCombinations(input_digit) {
+// function letterCombinations(input_digit) {
   //Complete the function
-}
+const digitToLetters = {
+  '2': 'abc',
+  '3': 'def',
+  '4': 'ghi',
+  '5': 'jkl',
+  '6': 'mno',
+  '7': 'pqrs',
+  '8': 'tuv',
+  '9': 'wxyz'
+};
 
-module.exports = letterCombinations;
+function letterCombinations(digits) {
+  if (digits.length === 0) {
+    return [];
+  }
+
+  const combinations = [''];
+  
+  for (let digit of digits) {
+    const letters = digitToLetters[digit];
+    const newCombinations = [];
+    
+    for (let combination of combinations) {
+      for (let letter of letters) {
+        newCombinations.push(combination + letter);
+      }
+    }
+    
+    combinations.length = 0; // Clear the previous combinations
+    combinations.push(...newCombinations);
+  }
+  
+  return combinations;
+}
