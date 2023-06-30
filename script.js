3edcc709c0eb143c14ffebeb9c774dbb51351db9
Vishakha02D2 <120273836+Vishakha02D2@users.noreply.github.com>
2023-06-30 23:48:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,50 @@
-// function letterCombinations(input_digit) {
+function letterCombinations(input_digit) {
   //Complete the function
-const digitToLetters = {
-  '2': 'abc',
-  '3': 'def',
-  '4': 'ghi',
-  '5': 'jkl',
-  '6': 'mno',
-  '7': 'pqrs',
-  '8': 'tuv',
-  '9': 'wxyz'
-};
+	// Mapping of digits to letters
+  const mapping = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz'
+  };
 
-function letterCombinations(digits) {
-  if (digits.length === 0) {
+  // Base case: if input is empty, return an empty array
+  if (input_digit.length === 0) {
     return [];
   }
 
-  const combinations = [''];
-  
-  for (let digit of digits) {
-    const letters = digitToLetters[digit];
-    const newCombinations = [];
-    
+  // Initialize the combinations array with an empty string
+  let combinations = [''];
+
+  // Iterate through each digit in the input string
+  for (let digit of input_digit) {
+    // Get the corresponding letters for the current digit
+    const letters = mapping[digit];
+
+    // Create a temporary array to store the updated combinations
+    let temp = [];
+
+    // Iterate through each existing combination and append each letter
+    // from the current digit to generate new combinations
     for (let combination of combinations) {
       for (let letter of letters) {
-        newCombinations.push(combination + letter);
+        temp.push(combination + letter);
       }
     }
-    
-    combinations.length = 0; // Clear the previous combinations
-    combinations.push(...newCombinations);
+
+    // Update the combinations array with the new combinations
+    combinations = temp;
   }
-  
+
+  // Sort the combinations lexicographically
+  combinations.sort();
+
+  // Return the resulting combinations array
   return combinations;
 }
+
+module.exports = letterCombinations;
