97593a2678863c9814aa5f4f7658506a1473cecc
Vishakha02D2 <120273836+Vishakha02D2@users.noreply.github.com>
2023-06-30 23:51:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
-// function letterCombinations(input_digit) {
-  //Complete the function
-const digitToLetters = {
-  '2': 'abc',
-  '3': 'def',
-  '4': 'ghi',
-  '5': 'jkl',
-  '6': 'mno',
-  '7': 'pqrs',
-  '8': 'tuv',
-  '9': 'wxyz'
-};
-
 function letterCombinations(digits) {
-  if (digits.length === 0) {
-    return [];
+	const digitToLetters = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz',
+    '0': '0',
+    '1': '1',
+  };
+
+  const combinations = [];
+
+  function backtrack(index, currentCombination) {
+    if (index === digits.length) {
+      combinations.push(currentCombination);
+      return;
+    }
+
+    const digit = digits[index];
+    const letters = digitToLetters[digit];
+
+    for (let i = 0; i < letters.length; i++) {
+      const letter = letters[i];
+      backtrack(index + 1, currentCombination + letter);
+    }
   }
 
-  const combinations = [''];
-  
-  for (let digit of digits) {
-    const letters = digitToLetters[digit];
-    const newCombinations = [];
-    
-    for (let combination of combinations) {
-      for (let letter of letters) {
-        newCombinations.push(combination + letter);
-      }
-    }
-    
-    combinations.length = 0; // Clear the previous combinations
-    combinations.push(...newCombinations);
+  if (digits.length > 0) {
+    backtrack(0, '');
   }
-  
+
   return combinations;
+  //Complete the function
 }
+
+module.exports = letterCombinations;
